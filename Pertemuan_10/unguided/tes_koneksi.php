fdaf3357f5c8fb4d441a33afbb727fa7c4cfcf6e
zclimaxsp <151652931+zclimaxsp@users.noreply.github.com>
2026-01-04 16:29:29 +0700

    menambahkan laporan praktikum 10

---
+++ Pertemuan_10/unguided/tes_koneksi.php
@@ -0,0 +1,12 @@
+<?php 
+
+// File: tes_koneksi.php 
+include "koneksi.php"; 
+ 
+if ($conn) { 
+    echo "   Koneksi ke database berhasil!"; 
+} else { 
+    echo "  Koneksi ke database gagal!"; 
+} 
+
+?>
